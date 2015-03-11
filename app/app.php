4f8580d8c55714110d419701d0c5dd81195562c5
Jonathan Lin & Brian Kropff <student@epicodus.com>
2015-03-11 11:45:32 -0700

    Make a website for our function. Can now play online with two players

--- app/app.php
+++ app/app.php
@@ -8,12 +8,14 @@
         'twig.path'=>__DIR__."/../views"
     ));
 
-    $app->get("/", function() use($app){
+    $app->get("/", function() use ($app){
         return $app['twig']->render('front.twig');
     });
 
-    $app->get ("/playgame", function() use ($app){
-        return $app['twig']->render('playgame.twig');    
+    $app->get("/playgame", function() use ($app){
+        $play1 = new RockPaperScissors();
+        $results = $play1->returnWinner($_GET['input1'], $_GET['input2']);
+        return $app['twig']->render('playgame.twig', array('winner' => $results));
     });
 
 
